a99f3272729d25dde1d10cab9ef7884eaaa22a3e
Daniel Costa <dancosta.id@gmail.com>
2022-04-09 10:20:02 -0300

    added and learned a bit about props

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,17 +1,14 @@
 import './ExpenseItem.css'
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2022, 4, 9)
-  const expeneseTitle = 'Car Insurance'
-  const expenseAmount = 204.67
+const ExpenseItem = (props) => {
 
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>{expeneseTitle}</h2>
+        <h2>{props.title}</h2>
       </div>
-      <div className='expense-item__price'>${expenseAmount}</div>
+      <div className='expense-item__price'>${props.amount}</div>
     </div>
   );
 };
