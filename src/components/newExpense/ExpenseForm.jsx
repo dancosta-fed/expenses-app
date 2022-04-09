b7d243ae1e6b2cc4f16e076643195021864b2b98
Daniel Costa <dancosta.id@gmail.com>
2022-04-09 16:57:39 -0300

    figured how to clear the form

--- src/components/newExpense/ExpenseForm.jsx
+++ src/components/newExpense/ExpenseForm.jsx
@@ -35,11 +35,18 @@ const ExpenseForm =  () => {
 
     const expenseData = {
       title: userInput.enteredTitle,
-      amount: userInput.enteredTitle,
+      amount: userInput.enteredAmount,
       date: userInput.enteredDate
     }
 
     console.log(expenseData)
+    // setUserInput('')
+    setUserInput({
+      enteredTitle: '',
+      enteredAmount: '',
+      enteredDate: ''
+    })
+ 
   }
 
   return (
@@ -48,17 +55,17 @@ const ExpenseForm =  () => {
 
           <div className="new-expense__control">
             <label>Title</label>
-            <input type="text" onChange={titleChangeHandler}/>
+            <input type="text" value={userInput.enteredTitle} onChange={titleChangeHandler}/>
           </div>
 
           <div className="new-expense__control">
             <label>Amount</label>
-            <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+            <input type="number" value={userInput.enteredAmount}  min="0.01" step="0.01" onChange={amountChangeHandler} />
           </div>
 
           <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" min="2022-01-01" max="2023-12-31" onChange={dateChangeHandler}/>
+            <input type="date" value={userInput.enteredDate} min="2022-01-01" max="2023-12-31" onChange={dateChangeHandler}/>
           </div>
         </div>
         <div className="new-expense__actions">
